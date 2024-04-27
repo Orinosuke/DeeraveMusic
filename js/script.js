5744a7c98a3777bd4c75f2363285f3a3c432f6e1
Sebastian Musiał <outkaze@OUTKAZE-3.local>
2024-04-27 17:15:59 +0200

    added media queries

--- js/script.js
+++ js/script.js
@@ -4,7 +4,7 @@ document.addEventListener("DOMContentLoaded", function () {
 	const navList = document.querySelector(".navbar-collapse");
 
 	function addShadow() {
-		if (window.scrollY >= 300) {
+		if (window.scrollY >= 50) {
 			nav.classList.add("shadow-bg");
 		} else {
 			nav.classList.remove("shadow-bg");
